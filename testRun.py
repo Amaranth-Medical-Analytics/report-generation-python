e9060080addc39eee9cf3dcb529ad611b45bd764
pranalis18 <46811834+pranalis18@users.noreply.github.com>
2023-09-29 09:08:15 +0530

    Report data generation scripts

---
+++ testRun.py
@@ -0,0 +1,3 @@
+from reportCleanup import postReportStats
+
+postReportStats('1516PT1', '/media/user/disk31/TCGA_report/SJRI/', 'SJRI', 'BRCA', '/media/user/disk31/TCGA_report/AIReport/config.json')
